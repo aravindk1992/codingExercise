c7c6e68ca3486bb4357b061e552f29ed4f8863d0
Arvind Ramesh <ArvindRamesh@Aravinds-MBP.fios-router.home>
2016-03-29 11:09:38 -0400

    maxDepth

---
+++ maxDepth.py
@@ -0,0 +1,20 @@
+
+
+"""Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+"""
+
+
+
+
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0 
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) +1
